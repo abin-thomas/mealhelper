71d81ea1e896d5346b5ab1dbe6d77ca74b5991af
abinthomas-Dev <abinthomas.dev@gmail.com>
2020-05-10 16:36:50 -0400

    Updates

---
+++ precache-manifest.22d326b60fb29e747e99b003501db1f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572e163c39f7cd2318a0c97e225f2a30",
+    "url": "/mealhelper/index.html"
+  },
+  {
+    "revision": "e280a6d3618bcd34e005",
+    "url": "/mealhelper/static/css/main.bc4f9902.chunk.css"
+  },
+  {
+    "revision": "8f68697edae20b2a05ae",
+    "url": "/mealhelper/static/js/2.59d0a58b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mealhelper/static/js/2.59d0a58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e280a6d3618bcd34e005",
+    "url": "/mealhelper/static/js/main.39d7e775.chunk.js"
+  },
+  {
+    "revision": "7894e20251f350e1165d",
+    "url": "/mealhelper/static/js/runtime-main.63524446.js"
+  }
+]);
