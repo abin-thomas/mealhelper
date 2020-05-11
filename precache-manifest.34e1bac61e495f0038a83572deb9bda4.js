ba8f2ffad9512cb81aa09c7edf59cb89dd9cb7d5
abinthomas-Dev <abinthomas.dev@gmail.com>
2020-05-10 20:44:01 -0400

    Updates

--- precache-manifest.34e1bac61e495f0038a83572deb9bda4.js
+++ precache-manifest.34e1bac61e495f0038a83572deb9bda4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a9a215e2560febedb2d8da7d25eaad21",
+    "revision": "2de2aa862003c4c5be8ca99d5af80f16",
     "url": "/mealhelper/index.html"
   },
   {
-    "revision": "7b89b9d83b459657f750",
-    "url": "/mealhelper/static/css/main.8bab51db.chunk.css"
+    "revision": "413352e9e646f6f04503",
+    "url": "/mealhelper/static/css/main.30b78435.chunk.css"
   },
   {
     "revision": "8f68697edae20b2a05ae",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mealhelper/static/js/2.59d0a58b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7b89b9d83b459657f750",
+    "revision": "413352e9e646f6f04503",
     "url": "/mealhelper/static/js/main.74a2138d.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mealhelper/precache-manifest.7c8eec427d0175dae0a67f063640ac13.js"
+  "/mealhelper/precache-manifest.34e1bac61e495f0038a83572deb9bda4.js"
 );
 
 self.addEventListener('message', (event) => {
